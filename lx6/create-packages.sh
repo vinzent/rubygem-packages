86ce619ca9ca8c66aa10b021d18890f046947b74
Thomas Mueller <mueller@puzzle.ch>
2015-08-25 11:33:50 +0200

    Initial lx6 packages

---
+++ lx6/create-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e 
+
+if ! which gem; then
+  yum install rubygems -y
+fi
+
+if ! which bundle; then
+  gem install bundler
+fi
+
+rpm -q ruby-devel >/dev/null 2>&1 || yum install ruby-devel -y -q
+rpm -q gcc >/dev/null 2>&1 || yum install gcc -y -q
+rpm -q rpm-build >/dev/null 2>&1 || yum install rpm-build -y -q
+
+if ! which fpm; then
+  bundle install
+fi
+
+bundle package
+
+find vendor/cache -name '*.gem' | xargs -rn1 fpm -d ruby -d rubygems \
+  --prefix /usr/share/gems \
+  --gem-bin-path /usr/bin \
+  --epoch 0 --iteration 0.el7 -s gem -t rpm
+
